245f31520a07147962ec914b50b8ea3a4ce124d8
Matthew W. Thomas <matthew@mwt.me>
2023-03-08 13:27:29 -0600

    clean up script and generate font db

--- generate.bash
+++ generate.bash
@@ -3,25 +3,22 @@ TINYTEX_VERSION="2023.03"
 TEXDIR="vtex"
 
 # Store working directory for later
-ORIGINAL_WD=$(pwd)
-
-# download and extract TinyTeX
+ORIGINAL_WD="$(pwd)"
 
 TINYTEX_URL="https://github.com/rstudio/tinytex-releases/releases/download/v$TINYTEX_VERSION/TinyTeX-0-v$TINYTEX_VERSION"
-wget --progress=dot:giga -O TinyTeX.tar.gz ${TINYTEX_URL}.tar.gz
 
-rm -rf $TEXDIR
-tar xzf TinyTeX.tar.gz
-mv .TinyTeX $TEXDIR
-rm TinyTeX.tar.gz
+# download and extract TinyTeX
+rm -rf "$TEXDIR"
+wget --progress=dot:giga -O- "${TINYTEX_URL}.tar.gz" | tar -xzC "./"
+mv ".TinyTeX" "$TEXDIR"
 
-# install requred 
+# install requred
 cd $TEXDIR/bin/*/
 ./tlmgr install latex-bin tools kvoptions etoolbox pdftexcmds infwarerr everysel parskip hyperref geometry sectsty ragged2e enumitem fontawesome markdown fontspec paralist amsmath amsfonts csvsimple fancyvrb pgf gobble epstopdf-pkg
 
-# create symlink for pdflatex
-ln -s pdftex pdflatex
+# generate font names database for lualatex (speedup generation later)
+./luaotfload-tool --update && echo "LuaLaTeX font database generated (exit: $?)" || echo "LuaLaTeX font database generation failed (exit: $?)"
 
 # package into final archive
-cd ${ORIGINAL_WD}
-tar -czvf vtex.tar.gz ${TEXDIR}
+cd "${ORIGINAL_WD}"
+tar -czf "vtex.tar.gz" "${TEXDIR}" && echo "vtex.tar.gz generated (exit: $?)" || echo "vtex.tar.gz generation failed (exit: $?)"
